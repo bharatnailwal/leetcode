e0d79ccd008db08352eb191c93dd7fb34014bf4f
Bharat Nailwal <42619150+bharatnailwal@users.noreply.github.com>
2022-05-27 23:50:23 +0530

    35. Search Insert Position

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,15 @@
+##Binay Search
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) -1
+        while l <= r:
+            mid = (l + r) // 2            
+            if target == nums[mid]:
+                return mid
+            if target > nums[mid]:
+                l = mid+1
+            else:
+                r = mid -1
+        return l
